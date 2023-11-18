b95d542bac449d2fcac86ffa677d0553240be234
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2023-11-18 18:08:23 +0300

    .

--- packages/contracts/contracts/axiom-v2-contracts/script/goerli/deploy_v2_query.sh
+++ packages/contracts/contracts/axiom-v2-contracts/script/goerli/deploy_v2_query.sh
@@ -2,8 +2,9 @@
 
 cd $(git rev-parse --show-toplevel)
 source .env
+echo $PRIVATE_KEY
 
-forge script script/goerli/AxiomV2QueryDeploy.s.sol:AxiomV2QueryDeploy --private-key $GOERLI_PRIVATE_KEY --rpc-url $GOERLI_RPC_URL --force --verify --etherscan-api-key $ETHERSCAN_API_KEY -vvvv --watch --broadcast
+forge script script/goerli/AxiomV2QueryDeploy.s.sol:AxiomV2QueryDeploy --private-key $PRIVATE_KEY --rpc-url $GNOSIS_RPC_URL --force --verify --etherscan-api-key $GNOSISSCAN_API_KEY -vvvv --watch --broadcast
 
 # if goerli slow and fails to verify, https://github.com/foundry-rs/foundry/issues/2435 
 #forge script script/goerli/AxiomV2QueryDeploy.s.sol:AxiomV2QueryDeploy --sender $SENDER_ADDRESS --keystore $KEYSTORE_PATH --rpc-url $GOERLI_RPC_URL --verify --etherscan-api-key $ETHERSCAN_API_KEY -vvvv --resume --wtach
