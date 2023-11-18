aa8c062db9c58bc5fd1012f20fb798868702cd53
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2023-11-18 16:57:35 +0300

    .

--- packages/contracts/contracts/sender/HeaderSender.sol
+++ packages/contracts/contracts/sender/HeaderSender.sol
@@ -6,6 +6,7 @@ import {AxiomV2Client} from "./AxiomV2Client.sol";
 import {HyperlaneMessageSender} from "./HyperlaneMessageSender.sol";
 import {IGateWay} from "./interfaces/IGateWay.sol";
 import {Bytes32ToString} from "./helpers/Bytes32ToString.sol";
+import {IAxiomV2State} from "./interfaces/core/IAxiomV2State.sol";
 
 contract HeaderSender is
     BasicMessageSender,
@@ -14,14 +15,43 @@ contract HeaderSender is
 {
     event CCIPMessageSent();
     IGateWay public gateway;
+    IAxiomV2State public headerverifier;
 
     constructor(
         address _router,
         address _link,
         address _outbox,
         address _gateway,
+        address _headerverifier,
         uint64 _callbackSourceChainId
     ) BasicMessageSender(_router, _link) HyperlaneMessageSender(_outbox) {
         gateway = IGateWay(_gateway);
+        headerverifier = IAxiomV2State(_headerverifier);
+    }
+
+    /**
+     * prevHash : 28557DED6224E6E220F8B0356D340FB034537685829425674DA6FCE9FDC1CD08
+     * root : A37A3EA88EB4C6DC014DCBC39B43AC3560A2C4F7D893D35C634A708D36284D3A
+     * numFinal : 384
+     * https://goerli.etherscan.io/tx/0x2877b686859d8efcc27c394db5aeb1f4829cfce34b2b58c138828444157e468a#eventlog
+     */
+    function getpmmr(
+        bytes32 prevHash,
+        bytes32 root,
+        uint32 numFinal,
+        bool bridge
+    ) public {
+        bytes pmmr = headerverifier.historicalRoots[
+            keccak256(abi.encodePacked(prevHash, root, numFinal))
+        ];
+
+        // use CCIP - arby goerli
+        if (bool == 0) {
+            sendMessage(6101244977088475029, receiver, pmmr);
+        }
+        // use Hyperlane
+        else if (bool == 1) {
+            sendViaHyperlane(1442, receiver, pmmr);
+        }
     }
 }
